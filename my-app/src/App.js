062ce2868b7d336095f3e042e98d780ed55ccd88
rachanark <rachanarkul@gmail.com>
2018-05-07 18:19:06 +0530

    up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import CategoryList from './container/CategoryList'
-import SubCategoryList from './container/SubCategoryList'
+import CategoryList from './container/CategoryList';
+import SubCategoryList from './container/SubCategoryList';
+
+
+import ProductPage from './container/ProductPage';
+import MasterPage from './container/MasterPage';
+import NotFoundPage from './container/NotFoundPage';
+import UploadImage from './components/UploadImage/UploadImage';
+import ImageCarousel from './components/ImageCarousel/ImageCarousel';
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+
+import { Switch, Route } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
@@ -13,6 +26,23 @@ class App extends Component {
         <h2>Add Product
         </h2>
          <SubCategoryList />
+
+
+        <hr />
+        <hr />
+        <hr />
+        <hr />
+        Below added componenets are just for testing. Use them as required!
+        <hr />
+        <hr />
+        <hr />
+        <hr />
+          <UploadImage />
+        <hr />
+        <hr />
+          <ImageCarousel />
+        <hr />
+        <hr />
       </div>
     );
   }
