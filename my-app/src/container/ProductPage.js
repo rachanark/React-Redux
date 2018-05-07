004f2971f3a8b7c8b0f01d89a51d304bf3cc81f2
rachanark <rachanarkul@gmail.com>
2018-05-07 18:23:41 +0530

    Create ProductPage.js

---
+++ my-app/src/container/ProductPage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class ProductPage extends Component {
+
+
+    render() {
+        return (
+            <div>
+                Product info - Add / Edit
+            </div>
+        );
+    }
+
+}
+
+
+export default ProductPage;
